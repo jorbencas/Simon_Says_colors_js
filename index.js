1bb322fc42994f6b6bbc532e2f1321cda17c7370
jorbencas <jorbencas@gmail.com>
2022-11-07 22:27:16 +0100

    arrrrggggg

--- index.js
+++ index.js
@@ -1,49 +1,90 @@
 let speed = 200;
 let level = 1;
-let colors = ['red', 'yellow','green','blue'];
+let colors = ["red", "yellow", "green", "blue"];
 let plays = [];
 let plaeysPlays = [];
-function start(){
-    let board = document.getElementById('board');    
-    board.innerHTML = colors.map( (color) => {
-        return `<li onclick='play("${color}")'>${color}</li>`;
-    }).join('\n');
+let maxLevel = 100;
+function start(event) {
+  //   sequence = new Array(maxLevel)
+  //     .fill(0)
+  //     .map((n) => Math.floor(Math.random() * 4));
+  //   console.log("====================================");
+  //   console.log(sequence);
+  //   console.log("====================================");
+  event.target.style.display = "none";
+  document.getElementById("board").style.display = "flex";
+  document.getElementById("level").style.display = "block";
+  printColors();
+  nextPlay();
+}
+
+async function nextPlay() {
+  if (level === 15 || level === 20) {
+    colors.push("olive");
+    colors.push("pink");
+    printColors();
+  } else if (level === 10) {
+    speed = speed - 200;
+  } else if (level == 1) {
+    //let color = colors[Math.floor(Math.random() * colors.length)];
+  } else if (level > 1) {
+    speed = speed - 100;
+  }
+  let color = colors[Math.floor(Math.random() * colors.length)];
+  plays.push(color);
+  plaeysPlays = plays;
+  Array.from(document.getElementsByTagName("li")).forEach((e) => {
+    e.onclick = null;
+  });
+
+  await bucle();
+  await console.log("bucle");
+
+  Array.from(document.getElementsByTagName("li")).forEach((e) => {
+    console.log("fu");
+    e.onclick = `play('${e.id}')`;
+  });
+}
+async function bucle() {
+  await plays.map(async (color) => {
+    await setTimeout(async () => {
+      await console.log("opacity(1)");
+      document.getElementById(`${color}`).style.filter = "opacity(1)";
+    }, speed);
+    await setTimeout(async () => {
+      await console.log("opacity(0.2)");
+
+      document.getElementById(`${color}`).style.filter = "opacity(0.2)";
+    }, speed);
+  });
+}
+
+const waitFor = () => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, speed);
+  });
+};
+
+function play(color) {
+  console.log("====================================");
+  console.log("Hola mUndo");
+  console.log("====================================");
+  if (plaeysPlays.shift() !== color || level == maxLevel) {
+    plaeysPlays = [];
+    level = 0;
+  } else {
+    plaeysPlays.unshift();
+    level++;
+    document.getElementById("level").innerText = `Nivel ${level}`;
     nextPlay();
+  }
 }
 
-function nextPlay(){
-    plays.forEach( (color) => {
-        setTimeout(() => {
-            document.getElementById(`${color}`).onclick = null;
-            document.getElementById(`${color}`).style.color = color;
-        }, speed);
-    }); 
-
-    plays.forEach( (color) => {
-        document.getElementById(`${color}`).onclick = `play('${color}')`;
-        document.getElementById(`${color}`).style.color = 'black';
-    }); 
-    if{level === 10}{
-        speed = speed - 2;
-    } else if{level === 15}{
-        // 2 colors mas
-    } else if{level === 20}{
-        // 2 colors mas
-    }
-    let color = colors[Math.floor(Math.random() * colors.length)];
-    plays.push(color);
-}
-
-function play(color){
-    if(plaeysPlays.length == plays.length){
-        if(plays.filter((e,i) => plaeysPlays[i] === e).length <  plays.length){
-            plaeysPlays = [];
-            level++;
-            setTimeout(() => {
-                nextPlay()
-            }, 4000);
-        }
-    } else{
-        plaeysPlays.push(color);
-    }
+function printColors() {
+  let board = document.getElementById("board");
+  board.innerHTML = colors
+    .map((color) => {
+      return `<li style='background-color:${color};' id='${color}' onclick='play("${color}")'>${color}</li>`;
+    })
+    .join("");
 }
