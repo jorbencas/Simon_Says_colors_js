1da8fe5ef0fef1810234c85afd4ec09a696e87ac
jorbencas <jorbencas@gmail.com>
2022-11-08 22:26:05 +0100

    do it

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 let speed = 2000;
 let level = 1;
-let colors = ["red", "yellow", "green", "blue"];
+let colorList = {
+  red: "eb4646",
+  yellow: "fbfb46",
+  green: "008046",
+  blue: "000046",
+  teal: "0fc2c5",
+  Salmon: "FA8072",
+};
+let colors = Object.keys(colorList).reverse().slice(2).reverse();
+
 let plays = [];
 let plaeysPlays = [];
 let maxLevel = 100;
+function playGame(color) {
+  console.log("====================================");
+  console.log("Hola mUndo " + color);
+  console.log("====================================");
+
+  if (plaeysPlays.length == 0 || level == maxLevel) {
+    plaeysPlays = [];
+    level = 1;
+  } else if (plaeysPlays.shift() === color) {
+    if (plays.length > 1) plaeysPlays.unshift();
+    level++;
+    document.getElementById("level").innerText = `Nivel ${level}`;
+    nextPlay();
+  } else {
+    document.getElementById("level").innerText = `Error `;
+    plaeysPlays = [];
+    level = 1;
+  }
+}
+
 function start(event) {
   //   sequence = new Array(maxLevel)
   //     .fill(0)
@@ -20,8 +49,8 @@ function start(event) {
 
 async function nextPlay() {
   if (level === 15 || level === 20) {
-    colors.push("olive");
-    colors.push("pink");
+    colors.push("teal");
+    colors.push("Salmon");
     printColors();
   } else if (level === 10) {
     speed = speed - 200;
@@ -32,60 +61,38 @@ async function nextPlay() {
   }
   let color = colors[Math.floor(Math.random() * colors.length)];
   plays.push(color);
-  plaeysPlays = plays;
-  Array.from(document.getElementsByTagName("li")).forEach((e) => {
-    e.onclick = null;
-  });
-
-    await bucle(),
-    await setTimeout(async () => {
-        Array.from(document.getElementsByTagName("li")).forEach(async (e) => {
-            console.log("fu fin");
-            e.onclick = `play('${e.id}')`;
-        });
-    }, speed);
+  plaeysPlays = [...plays];
+  console.log("====================================");
+  console.log(plays);
+  console.log("====================================");
+  document.getElementById("board").style.cursor = "disabled";
+  await bucle();
+  document.getElementById("board").style.cursor = "default";
 }
 
 async function bucle() {
-  plays.forEach((color, i) => {
-    await ( async (i) => {setTimeout((i) => {
-      console.log("opacity(1)" + i + (speed));
-      document.getElementById(`${color}`).style.filter = "opacity(1)";
-    }, speed  * ( i + 1))})(i);
-    await ( async (i) => {setTimeout((i) => {
-      console.log("opacity(0.2)" + i + (speed));
-      document.getElementById(`${color}`).style.filter = "opacity(0.2)";
-    }, speed * ( i + 1))})(i)
+  plays.forEach(async (color, i) => {
+    await waitFor(speed * (i + 1));
+    document.getElementById(`${color}`).style.color = "white";
+    document.getElementById(`${color}`).style.backgroundColor =
+      "#" + colorList[color];
+    await waitFor(speed * (i + 2));
+    document.getElementById(`${color}`).style.color = color;
+    document.getElementById(`${color}`).style.backgroundColor = "transparent";
   });
-  
 }
 
-const waitFor = () => {
+const waitFor = (ms) => {
   return new Promise((resolve) => {
-    setTimeout(resolve, speed);
+    setTimeout(resolve, ms);
   });
 };
-
-function play(color) {
-  console.log("====================================");
-  console.log("Hola mUndo");
-  console.log("====================================");
-  if (plaeysPlays.shift() !== color || level == maxLevel) {
-    plaeysPlays = [];
-    level = 0;
-  } else {
-    plaeysPlays.unshift();
-    level++;
-    document.getElementById("level").innerText = `Nivel ${level}`;
-    nextPlay();
-  }
-}
 
 function printColors() {
   let board = document.getElementById("board");
   board.innerHTML = colors
     .map((color) => {
-      return `<li style='background-color:${color};' id='${color}' onclick='play("${color}")'>${color}</li>`;
+      return `<li style='border:2px solid ${color}; color:${color};' id='${color}' onclick='playGame("${color}")'>${color}</li>`;
     })
     .join("");
 }
